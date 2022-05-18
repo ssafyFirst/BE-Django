d97769ff7961eab0c54a35a4f430e52083402b97
YoonKyu <newjeff@naver.com>
2022-05-18 22:24:46 +0900

    dump json

--- movies/models.py
+++ movies/models.py
@@ -6,19 +6,14 @@ class Genre(models.Model):
 
 
 class Movie(models.Model):
+    title = models.CharField(max_length=100)
     adult = models.BooleanField()
-
-    backdrop_path = models.CharField(max_length=200)
-    genres_ids = models.ManyToManyField(Genre, related_name='movie')
-    original_language = models.CharField(max_length=30)
-    original_title = models.CharField(max_length=50)
-    overview = models.TextField()
     popularity = models.FloatField()
     poster_path = models.CharField(max_length=200)
-
-    release_date = models.DateField()
-    
-    title = models.CharField(max_length=100)
-    video = models.BooleanField()
+    backdrop_path = models.CharField(max_length=200, null=True)
+    genres = models.ManyToManyField(Genre, related_name='movie', blank=True)
+    original_language = models.CharField(max_length=30, null=True)
+    released_date = models.DateField()
     vote_average = models.FloatField()
     vote_count = models.IntegerField()
+    overview = models.TextField()
