ac877211d9a3fd45a38105eb3370ceb7e02a6436
YoonKyu <newjeff@naver.com>
2022-06-01 10:36:29 +0900

    회원정보 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup2/<username>/', csrf_exempt(views.signup2)),
     path('profile/<username>/', views.profile),
+    # 회원수정 업데이트 url
+    path('update/<username>/', views.update)
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,12 @@ from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
 
-from .serializers import ProfileSerializer
+
+from .serializers import ProfileSerializer, CustomRegisterSerializer
 from rest_framework.response import Response
+
+from accounts import serializers
+
 # Create your views here.
 
 User = get_user_model()
@@ -16,3 +20,13 @@ def profile(request, username):
     user = get_object_or_404(User, username=username)
     serializer = ProfileSerializer(user)
     return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def update(request, username):
+    user = get_object_or_404(User, username=username)
+    if user == request.user:
+        serializer = CustomRegisterSerializer(instance=user, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
